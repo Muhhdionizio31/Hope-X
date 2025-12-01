5d3cd62c828c27c98785ade3c112597c6ce83b73
Murilo Dionizio <murilomelodionizio@gmail.com>
2025-12-01 09:45:27 -0300

    Arrumando a divisoria das avaliaçoes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS
 import smtplib
 from email.message import EmailMessage
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
